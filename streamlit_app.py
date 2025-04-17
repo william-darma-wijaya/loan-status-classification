5b186c5bf6758d0217e175d4c202acf6922a0f38
william-darma-wijaya <94919753+william-darma-wijaya@users.noreply.github.com>
2025-04-17 14:14:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
-
+import joblib
+import pandas as pd
 st.title('Loan Status Prediction')
+
+# Load model
+model = joblib.load("savedPickle/model.pkl")
+
+# Load encoders
+labelEncoders = joblib.load("savedPickle/label_encoders.pkl")
+ordinalEncoders = joblib.load("savedPickle/ordinal_encoders.pkl")
+
