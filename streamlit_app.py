620e0d080a69bb8a5b79b1dcf4063d481f07d088
william-darma-wijaya <94919753+william-darma-wijaya@users.noreply.github.com>
2025-04-17 15:54:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import joblib
 import pandas as pd
-st.title('Loan Status Prediction')
 
 # Load model
 model = joblib.load("savedPickle/model.pkl")
@@ -10,3 +9,9 @@ model = joblib.load("savedPickle/model.pkl")
 labelEncoders = joblib.load("savedPickle/label_encoders.pkl")
 ordinalEncoders = joblib.load("savedPickle/ordinal_encoders.pkl")
 
+def main():
+  st.title('Loan Status Prediction')
+  st.write("Insert your data below, then click **PREDICT**")
+
+if __name__ == "__main__":
+  main()
